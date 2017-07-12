eb40a1f01fbfcdb757c50fb9f4c9b62918fd057a
HelloBugs <330194227@qq.com>
2017-07-13 01:23:29 +0800

    commit roll-a-ball

---
+++ TheLandOfTheHunting/Assets/Scripts/PlayerContronal.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerContronal : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float speed;
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        rb.AddForce(moveHorizontal * speed, 0, moveVertical * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+}
