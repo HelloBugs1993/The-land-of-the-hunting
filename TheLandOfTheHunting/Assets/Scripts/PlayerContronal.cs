915f90756cc299169d14cf929d28b869a2860727
HelloBugs <330194227@qq.com>
2017-07-15 16:49:01 +0800

    提交学习进度

--- TheLandOfTheHunting/Assets/Scripts/PlayerContronal.cs
+++ TheLandOfTheHunting/Assets/Scripts/PlayerContronal.cs
@@ -1,15 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class PlayerContronal : MonoBehaviour
 {
     private Rigidbody rb;
     public float speed;
+    private int count;
+    public Text countText;
+    public Text winText;
     // Use this for initialization
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
     }
 
     // Update is called once per frame
@@ -26,6 +32,18 @@ public class PlayerContronal : MonoBehaviour
         if (other.gameObject.CompareTag("Pick Up"))
         {
             other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+            
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count： " + count.ToString();
+        if (count >= 8)
+        {
+            winText.text = "You Win!";
         }
     }
 }
